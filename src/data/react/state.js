af89c58856c245a96d4e916b023c007aab2001b5
Jo Blaszak <jblaszak@gmail.com>
2022-10-10 14:44:17 -0700

    Added most of state section for React.

--- src/data/react/state.js
+++ src/data/react/state.js
@@ -4,6 +4,28 @@ export const pageData = {
     it needs to re-render.`,
   cheats: [
     [
+      { h2: "Hooks" },
+      {
+        p: `Hooks are javascript functions that 'hook' into features in React like state and the overall component lifecycle. Hooks will always
+        begin with 'use' and cannot be used within a conditional.  Must be used at the root level of a component.`,
+      },
+      {
+        code: {
+          language: `javascript`,
+          code: `import { useState } from 'react';
+
+export default function MyComponent() {
+    const [count, setCount] = useState(0);
+
+    // not allowed
+    if (true) {
+        const [other, setOther] = useState(0);
+    }
+}`,
+        },
+      },
+    ],
+    [
       { h2: "Creating/Storing State" },
       {
         p: `useState gets destructured into two parts, a getter and a setter. It is important to always use the setter to alter the value of
@@ -16,6 +38,8 @@ export const pageData = {
 
 export default function MyComponent() {
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [complex, setComplex] = useState(complexStateGenerator);
 
     return (
         <>
@@ -23,6 +47,131 @@ export default function MyComponent() {
             <p>Count: {count}</p>
         </>
     );
+}
+
+function complexStateGenerator() {
+    // do something complex
+    return complexState;
+}`,
+        },
+      },
+    ],
+    [
+      { h2: "Using Previous State" },
+      {
+        p: `React will only update the state once the function calling the state has finished running. Can use function within
+        the state setting function that updates the previous value if needing to alter state in a way that is dependent on that
+        previous value.`,
+      },
+      {
+        code: {
+          language: `javascript`,
+          code: `import { useState } from 'react';
+
+export default function MyComponent() {
+    const [count, setCount] = useState(0);
+
+    // Only increases count by 1 because count variable only changed
+    // after function calling it ends
+    return (
+        <>
+            <button onClick={() => {
+                setCount(count + 1);
+                setCount(count + 1);
+                setCount(count + 1);
+            }}>
+                Increment!
+            </button>
+            <p>Count: {count}</p>
+        </>
+    );
+
+    // Increases count by 3
+    return (
+        <>
+            <button onClick={() => {
+                setCount(prevCount => prevCount + 1);
+                setCount(prevCount => prevCount + 1);
+                setCount(prevCount => prevCount + 1);
+            }}>
+                Increment!
+            </button>
+            <p>Count: {count}</p>
+        </>
+    );
+}`,
+        },
+      },
+    ],
+    [
+      { h2: "Setting State as Object or Array" },
+      {
+        p: `Since changing values in an array/object doesn't change the object itself, we have to pass a new object to
+        setState.`,
+      },
+      {
+        code: {
+          language: `javascript`,
+          code: `export default function MyComponent({startingCount = 0}) {
+    const [count, setCount] = useState({num: startingCount});
+    const [count2, setCount2] = useState([0,1,2,3]);
+
+    setCount({...count, otherCount: 0});
+    setCount2([...count2, 4, 5, 6]);
+
+    return <p>{count} and {count2}</p>;
+}`,
+        },
+      },
+    ],
+    [
+      { h2: "Lifting State Up" },
+      {
+        p: `Lifting state is a term for when you move the state to the lowest common ancestor.  This means that multiple components can
+        have access to the same value and setter functions.`,
+      },
+      {
+        code: {
+          language: `javascript`,
+          code: `export default function App() {
+    const [count, setCount] = useState(0);
+
+    // NOTE: both of these will have the same value when count changes since that is what is passed to
+    // the component!
+    return (
+        <>
+        <MyComponent count={count} setCount={setCount} />
+        <MyComponent count={count} setCount={setCount} />
+        </>
+    );
+}
+          
+function MyComponent({count, setCount}) {
+    return (
+        <>
+            <button onClick={setCount(count + 1)} >
+                Increment!
+            </button>
+            <p>Count: {count}</p>
+        </>
+    );
+}`,
+        },
+      },
+    ],
+    [
+      { h2: "Controlled Component" },
+      {
+        p: `When using React state to control the state of an input instead of the native elements controlling themselves you have a
+        controlled component. This is a common react pattern.`,
+      },
+      {
+        code: {
+          language: `javascript`,
+          code: `export default function MyInput() {
+    const [value, setValue] = useState('');
+
+    return <input type='text' value={value} onChange={(event) => setValue(event.target.value)} />;
 }`,
         },
       },

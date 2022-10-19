6d72da75c0a5a44413b16f2061be8ddeffb5e45e
Jo Blaszak <jblaszak@gmail.com>
2022-10-18 22:44:13 -0700

    Added to website theory.

--- src/data/theory/websites.js
+++ src/data/theory/websites.js
@@ -36,5 +36,66 @@ export const pageData = {
         link: `https://developer.mozilla.org/en-US/docs/Learn/Accessibility/WAI-ARIA_basics`,
       },
     ],
+    [
+      { h2: "Client-Server Model" },
+      {
+        p: `Most modern systems are designed using this paradigm where clients request data or service from a server and servers provide that data
+        to the client.  NOTE: A server may also be a client in the sense that it recieves a request from a client but then needs to make a request
+        to a database for some data before it can send it to the original client. For the web, browsers are clients that request HTML pages from
+        servers.`,
+      },
+      {
+        p: `Clients - execute code quickly, users can see code, have low computing power, don't have direct access to database`,
+      },
+      {
+        p: `Servers - execute code slowly because they have to wait on network, implemenation details of code are hidden from end user,
+      "infinite" computing power as servers are scalable, quick and easy database access`,
+      },
+    ],
+    [
+      { h2: "URL" },
+      {
+        p: `https://www.mycoolsite.com:443/myPage?someValue=100#pageSection`,
+      },
+      {
+        definitionList: [
+          { title: `https://`, definition: `Scheme or protocol to use` },
+          { title: `www`, definition: `Subdomain` },
+          { title: `mycoolsite.com`, definition: `Domain` },
+          { title: `.com`, definition: `Top Level Domain` },
+          {
+            title: `:443`,
+            definition: `Port, optional as browsers put it in for you (443 for https or 80 for http)`,
+          },
+          { title: `myPage`, definition: `Path` },
+          { title: `?someValue=100`, definition: `Query Parameters` },
+          {
+            title: `#pageSection`,
+            definition: `Tag in HTML to target for scroll behaviour`,
+          },
+        ],
+      },
+    ],
+    [
+      { h2: "Domain Name System (DNS)" },
+      {
+        p: `Used to convert the domain name to an IP address.  When a request is sent from your computer it goes
+        to a resolving name server that will talk to other servers and cache the names/ips for certain domains so they 
+        can more quickly serve them to you. If the server doesn't have the domain cached, it reaches out to a root name
+        server, a TLD (top level domain) name server, and eventually an authoritative name server to find the correct IP
+        address for the domain name.`,
+      },
+    ],
+    [
+      { h2: "TCP - Transmission Control Protocol" },
+      {
+        p: `Used to establish connections between a client and server on the internet. Uses the IP address to open a TCP connection
+        and sends data as packets. Establishes a connection using a three-way handshake (synchronization, synchronization acknowledgement,
+          and a final acknowledgement). Uses lots of error correction which increases latency and bandwidth.  `,
+      },
+      {
+        link: `https://www.spiceworks.com/tech/networking/articles/tcp-vs-udp/`,
+      },
+    ],
   ],
 }

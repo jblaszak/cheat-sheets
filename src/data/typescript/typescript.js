ee6fc4ec8c422758d172c501956e30be94faf652
Jo Blaszak <jblaszak@gmail.com>
2022-11-15 22:29:21 -0800

    Added more to typescript section.

--- src/data/typescript/typescript.js
+++ src/data/typescript/typescript.js
@@ -8,7 +8,7 @@ export const pageData = {
       { p: `All typescript files will have the '.ts' file extension.` },
       {
         code: {
-          language: `javascript`,
+          language: `typescript`,
           code: `let test = 123; // implicitly sets type to number
 test = '456';  // throws error
 
@@ -26,7 +26,7 @@ let test: any = 123; // allows for any type, only really useful if converting so
       { h2: `Custom Types` },
       {
         code: {
-          language: `javascript`,
+          language: `typescript`,
           code: `type NumString = number | string;
 let test: NumString = 123;
 
@@ -41,7 +41,7 @@ test = 'on'; // later on don't need to declare type anymore as it's already set`
       { h2: `Enums` },
       {
         code: {
-          language: `javascript`,
+          language: `typescript`,
           code: `enum State {
     On = 'on',
     Off = 'off'
@@ -56,7 +56,7 @@ console.log(test); // outputs 1 (since 1 represents the 2nd value in the enum)`,
       { h2: `Functions` },
       {
         code: {
-          language: `javascript`,
+          language: `typescript`,
           code: `// functions that return values should include type
 function add(x: number, y: number): number {
     return x + y;
@@ -65,6 +65,22 @@ function add(x: number, y: number): number {
 // functions that don't return should have type void
 function add(x: number, y: number): void {
     console.log(x + y);
+}
+
+let add: (x:number, y: number) => void {
+  console.log(x + y);
+}
+
+function f1(a: any) {
+  a.b(); // OK
+}
+
+function f2(a: unknown) {
+  a.b();  //Object is of type 'unknown'.
+}
+
+function fail(msg: string): never {
+  throw new Error(msg);
 }`,
         },
       },
@@ -73,14 +89,14 @@ function add(x: number, y: number): void {
       { h2: `Objects & Interfaces` },
       {
         code: {
-          language: `javascript`,
+          language: `typescript`,
           code: `interface Coder {
     name: string;
     course: string;
     age?: number; // optional entry
 }
 
-// interafaces are used to assign types to objects
+// interfaces are used to assign types to objects
 const bob: Coder = {
     name: 'bob',
     job: 'developer',
@@ -106,10 +122,80 @@ class SuperCoder implements Coder {
       },
     ],
     [
+      { h2: `Types vs Interfaces` },
+      {
+        code: {
+          language: `typescript`,
+          code: `type BirdType = {
+  wings: 2;
+};
+
+interface BirdInterface {
+  wings: 2;
+}
+
+const bird1: BirdType = { wings: 2 };
+const bird2: BirdInterface = { wings: 2 };
+const bird3: BirdInterface = bird1;`,
+        },
+      },
+      {
+        p: `Can both extend other interfaces and types.`,
+      },
+      {
+        code: {
+          language: `typescript`,
+          code: `type Owl = { nocturnal: true } & BirdType;
+type Robin = { nocturnal: false } & BirdInterface;
+
+interface Peacock extends BirdType {
+  colourful: true;
+  flies: false;
+}
+interface Chicken extends BirdInterface {
+  colourful: false;
+  flies: false;
+}
+
+let owl: Owl = { wings: 2, nocturnal: true };
+let chicken: Chicken = { wings: 2, colourful: false, flies: false };`,
+        },
+      },
+      {
+        p: `Interfaces are recommneded over types due to better error messages.`,
+      },
+      {
+        p: `Major difference between types and interfaces is that interfaces can be extended when declared a second time (open) while types 
+        can not (closed).`,
+      },
+      {
+        code: {
+          language: `typescript`,
+          code: `// Works fine! 
+interface Kitten {
+  purrs: boolean;
+}
+
+interface Kitten {
+  colour: string;
+}
+          
+// Below will throw errors!!!
+type Puppy = {
+  color: string;
+};
+
+type Puppy = {
+  toys: number;
+};`,
+        },
+      },
+    ],
+    [
       { h2: `Generics` },
       {
         code: {
-          language: `javascript`,
+          language: `typescript`,
           code: `const arr = [1,2,3,'4']; // typescript doesn't prevent this
 const arr = Array<number> = [1,2,3,'4']; // throws error
 const arr = Array<number> = [1,2,3]; // works fine!
@@ -142,10 +228,17 @@ class StringNumGetterSetter implements GetterSetter<string, number> {
           code: `npx create-react-app myAppName --template typescript`,
         },
       },
-      { p: `Typescript ` },
+      { p: `Add typescript to current react project with:` },
       {
         code: {
-          language: `javascript`,
+          language: `bash`,
+          code: `npm install --save typescript @types/node @types/react @types/react-dom @types/jest`,
+        },
+      },
+      { p: `Typescript for a component with props:` },
+      {
+        code: {
+          language: `typescript`,
           code: `interface CoolProps {
   foo: number;
   bar: string;
